12dc532d8700d317a4ae4a1e7494a22990441b1a
youssef <youssef.besbes@tuhh.de>
2026-09-20 21:02:08 +0200

    Improve mobile responsive design

--- backend/db.js
+++ backend/db.js
@@ -1,33 +1,23 @@
 const { Pool } = require("pg");
-
 require("dotenv").config();
 
-const pool = new Pool(
-  process.env.DATABASE_URL
-    ? {
-        connectionString:
-          process.env.DATABASE_URL,
+let pool;
 
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }
-    : {
-        user:
-          process.env.DB_USER,
-
-        host:
-          process.env.DB_HOST,
-
-        database:
-          process.env.DB_NAME,
-
-        password:
-          process.env.DB_PASSWORD,
-
-        port:
-          process.env.DB_PORT,
-      }
-);
+if (process.env.DATABASE_URL) {
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+  });
+}
 
 module.exports = pool;
